9ce1a331f910882481693a52567477a0f7fbaf7e
Bruno Petrikovski <petrikovskibruno@gmail.com>
2026-06-08 16:52:09 -0300

    Corrigir bandeiras dos paises

--- lib/flags.ts
+++ lib/flags.ts
@@ -1,48 +1,139 @@
 export const bandeiras: Record<string, string> = {
   México: "🇲🇽",
+  Mexico: "🇲🇽",
+
   "África do Sul": "🇿🇦",
+  "South Africa": "🇿🇦",
+
   "Coreia do Sul": "🇰🇷",
+  "Korea Republic": "🇰🇷",
+
   "República Tcheca": "🇨🇿",
+  Czechia: "🇨🇿",
+
   Canadá: "🇨🇦",
-  "Bósnia e Herzegovina": "🇧🇦",
-  "Estados Unidos": "🇺🇸",
-  Paraguai: "🇵🇾",
-  Haiti: "🇭🇹",
-  Escócia: "🏴󠁧󠁢󠁳󠁣󠁴󠁿",
-  Brasil: "🇧🇷",
-  Marrocos: "🇲🇦",
-  Alemanha: "🇩🇪",
-  "Costa do Marfim": "🇨🇮",
-  Argentina: "🇦🇷",
-  França: "🇫🇷",
-  Portugal: "🇵🇹",
-  Espanha: "🇪🇸",
-  Uruguai: "🇺🇾",
-  Bélgica: "🇧🇪",
-  Holanda: "🇳🇱",
-  Japão: "🇯🇵",
-  Suíça: "🇨🇭",
-  Colômbia: "🇨🇴",
-  Inglaterra: "🏴",
-  Croácia: "🇭🇷",
-  Turquia: "🇹🇷",
-  Austrália: "🇦🇺",
-  Equador: "🇪🇨",
-  Tunísia: "🇹🇳",
-  Irã: "🇮🇷",
-  Egito: "🇪🇬",
-  "Nova Zelândia": "🇳🇿",
-  Senegal: "🇸🇳",
-  Noruega: "🇳🇴",
-  Argélia: "🇩🇿",
-  Áustria: "🇦🇹",
-  Gana: "🇬🇭",
-  Panamá: "🇵🇦",
-  Uzbequistão: "🇺🇿",
+  Canada: "🇨🇦",
+
   Catar: "🇶🇦",
+  Qatar: "🇶🇦",
+
+  "Bósnia e Herzegovina": "🇧🇦",
+  "Bosnia and Herzegovina": "🇧🇦",
+
+  Suíça: "🇨🇭",
+  Switzerland: "🇨🇭",
+
+  Brasil: "🇧🇷",
+  Brazil: "🇧🇷",
+
+  Escócia: "🏴",
+  Scotland: "🏴",
+
+  Marrocos: "🇲🇦",
+  Morocco: "🇲🇦",
+
+  Haiti: "🇭🇹",
+
+  "Estados Unidos": "🇺🇸",
+  USA: "🇺🇸",
+
+  Paraguai: "🇵🇾",
+  Paraguay: "🇵🇾",
+
+  Turquia: "🇹🇷",
+  Türkiye: "🇹🇷",
+
+  Austrália: "🇦🇺",
+  Australia: "🇦🇺",
+
+  Alemanha: "🇩🇪",
+  Germany: "🇩🇪",
+
+  "Costa do Marfim": "🇨🇮",
+  "Côte d'Ivoire": "🇨🇮",
+
+  Equador: "🇪🇨",
+  Ecuador: "🇪🇨",
+
+  Curaçao: "🇨🇼",
+
+  Holanda: "🇳🇱",
+  Netherlands: "🇳🇱",
+
+  Japão: "🇯🇵",
+  Japan: "🇯🇵",
+
+  Suécia: "🇸🇪",
+  Sweden: "🇸🇪",
+
+  Tunísia: "🇹🇳",
+  Tunisia: "🇹🇳",
+
+  Espanha: "🇪🇸",
+  Spain: "🇪🇸",
+
+  Uruguai: "🇺🇾",
+  Uruguay: "🇺🇾",
+
   "Arábia Saudita": "🇸🇦",
+  "Saudi Arabia": "🇸🇦",
+
   "Cabo Verde": "🇨🇻",
-  Jordânia: "🇯🇴",
+
+  Bélgica: "🇧🇪",
+  Belgium: "🇧🇪",
+
+  Irã: "🇮🇷",
+  "IR Iran": "🇮🇷",
+
+  Egito: "🇪🇬",
+  Egypt: "🇪🇬",
+
+  "Nova Zelândia": "🇳🇿",
+  "New Zealand": "🇳🇿",
+
+  França: "🇫🇷",
+  France: "🇫🇷",
+
+  Senegal: "🇸🇳",
+
+  Noruega: "🇳🇴",
+  Norway: "🇳🇴",
+
   Iraque: "🇮🇶",
+  Iraq: "🇮🇶",
+
+  Argentina: "🇦🇷",
+
+  Argélia: "🇩🇿",
+  Algeria: "🇩🇿",
+
+  Áustria: "🇦🇹",
+  Austria: "🇦🇹",
+
+  Jordânia: "🇯🇴",
+  Jordan: "🇯🇴",
+
+  Inglaterra: "🏴",
+  England: "🏴",
+
+  Croácia: "🇭🇷",
+  Croatia: "🇭🇷",
+
+  Gana: "🇬🇭",
+  Ghana: "🇬🇭",
+
+  Panamá: "🇵🇦",
+  Panama: "🇵🇦",
+
+  Portugal: "🇵🇹",
+
+  Colômbia: "🇨🇴",
+  Colombia: "🇨🇴",
+
+  Uzbequistão: "🇺🇿",
+  Uzbekistan: "🇺🇿",
+
   "República Democrática do Congo": "🇨🇩",
+  "Congo DR": "🇨🇩",
 };
